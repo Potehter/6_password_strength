c5506e6a46c72eb3b01d6eea7891f4c6eebcc787
Dmitrii <poteher@gmail.com>
2018-01-06 01:25:34 +0300

    first commit. check password

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+Program can calculate strength of password. Scale of strenth is from 1 (the weakest) to 10 (the strongest). Requirements to password:
+- length
+- incoming of upper-case and lower-case letters, digits, special characters
+- exluded from black list of passwords
+Black list of passwords - is a txt file (passwords.txt) in run directory
+
+# How to use
+
+After run password_strength.py you have to input password, after welcoming phrase.
+
+example:
+'''
+python password_strength.py 
+Input password to check: 
+''' 
+
+output:
+'''
+Strength of your password: 1
+'''
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,56 @@
+import re
+
+
+def get_entrance_black_list(password):
+    try:
+        file = open('passwords.txt', 'r')
+    except IOError as e:
+        print('Cannot check password in black list')
+        return 0
+    else:
+        with file:
+            black_passwords = file.read()
+            list_black_passwords = black_passwords.split('\r\n')
+            if password in list_black_passwords:
+                return 0
+            else:
+                return 3
+
+
+def get_length_strength(password):
+    if len(password) > 10:
+        return 3
+    elif len(password) > 8:
+        return 2
+    elif len(password) > 5:
+        return 1
+    else:
+        return 0
+
+
+def get_character_diversity(password):
+    counter = 0
+    array_re_to_check = ['[a-z]', '[A-Z]', '\d', '\W']
+    for each_case in array_re_to_check:
+        match = re.search(each_case, password)
+        if match:
+            counter += 1
+    return counter
 
 
 def get_password_strength(password):
-    pass
+    strength_lenght = get_length_strength(password)
+    strength_diversity = get_character_diversity(password)
+    strength_from_black_list = get_entrance_black_list(password)
+    total_strength = sum((strength_lenght, strength_diversity, 
+                          strength_from_black_list))
+    return str(total_strength)
+
+
+def main():
+    password = input('Input password to check: ')
+    print('Strength of your password: ' + get_password_strength(password))
 
 
 if __name__ == '__main__':
-    pass
+    main()
